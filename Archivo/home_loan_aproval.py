1564b4e705bf36fd3d5480942f352a08157e8381
Sergio Nicolas Segui <sergionicolassegui@macbookdesergio.lan>
2024-03-09 13:32:17 +0000

    80 percent

--- Archivo/home_loan_aproval.py
+++ Archivo/home_loan_aproval.py
@@ -9,6 +9,9 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
 
 VARIABLES = ('Gender','Married','Dependents','Education','SelfEmployed','ApplicantIncome','CoapplicantIncome','LoanAmount','LoanAmountTerm',   'PropertyArea')
 
@@ -29,10 +32,11 @@ def distributions(dataframe):
     plt.show()
 
 
-def drop_outilers(dataframe, variable, max): 
- # Por ejemplo, el max es 1000
-  # Seleccionar las filas que cumplen con la condición
-  new_dataframe = dataframe.loc[dataframe[variable] <= max]
+def drop_outliers(dataframe, variable, max_value, min_value=None):
+  if min_value is not None:
+    new_dataframe = dataframe.loc[(dataframe[variable] <= max_value) & (dataframe[variable] >= min_value)]
+  else:
+    new_dataframe = dataframe.loc[dataframe[variable] <= max_value]
   return new_dataframe
   
 
@@ -125,6 +129,59 @@ def naive_bayes(df):
   # Mostrar el informe de clasificación
   # print('\nClassification Report:\n', classification_report(y_test, y_pred))
 
+def buildClassificatonTree(csv_path, target_column):
+  # Cargar datos desde el archivo CSV
+  data = pd.read_csv(csv_path)
+  
+  # Separar características (X) y variable objetivo (y)
+  X = data.drop(columns=[target_column])
+  y = data[target_column]
+  
+  # Dividir los datos en conjuntos de entrenamiento y prueba
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+  
+  # Construir el modelo de árbol de clasificación
+  tree_classifier = DecisionTreeClassifier()
+  
+  # Entrenar el modelo
+  tree_classifier.fit(X_train, y_train)
+  
+  # Predecir sobre el conjunto de prueba
+  y_pred = tree_classifier.predict(X_test)
+  
+  # Calcular la precisión del modelo
+  accuracy = accuracy_score(y_test, y_pred)
+  print("\nARBOL DE CLASIFICACION\n")
+  print("Accuracy: ", accuracy)
+  
+  return tree_classifier
+
+def buildKNNeighbors(csv_path, target_column, k = 5):
+  # Cargar datos desde el archivo CSV
+    data = pd.read_csv(csv_path)
+    
+    # Separar características (X) y variable objetivo (y)
+    X = data.drop(columns=[target_column])
+    y = data[target_column]
+    
+    # Dividir los datos en conjuntos de entrenamiento y prueba
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+    # Construir el modelo de clasificador kNN
+    knn_classifier = KNeighborsClassifier(n_neighbors=k)
+    
+    # Entrenar el modelo
+    knn_classifier.fit(X_train, y_train)
+    
+    # Predecir sobre el conjunto de prueba
+    y_pred = knn_classifier.predict(X_test)
+    
+    # Calcular la precisión del modelo
+    accuracy = accuracy_score(y_test, y_pred)
+    print("\nCLASIFICADOR KNN", "\n\nAccuracy: ", accuracy)
+    
+    return knn_classifier
+
 ## main
 def main():
 
@@ -133,9 +190,9 @@ def main():
   dataframe = pd.read_csv(archivo_csv)
   dataframe = one_hot_encoder(dataframe) # Convertir todos los valores categóricos en valores enteros
 #
-  dataframe = drop_outilers(dataframe, 'ApplicantIncome', 3000) # Eliminar los valores atípicos de la columna 'ApplicantIncome'
-  dataframe = drop_outilers(dataframe, 'LoanAmount', 350) # Eliminar los valores atípicos de la columna 'LoanAmount'
-  dataframe = drop_outilers(dataframe, 'CoapplicantIncome', 100000) # Eliminar los valores atípicos de la columna 'CoapplicantIncome'
+  dataframe = drop_outliers(dataframe, 'ApplicantIncome', max_value = 3000) # Eliminar los valores atípicos de la columna 'ApplicantIncome'
+  dataframe = drop_outliers(dataframe, 'LoanAmount', max_value = 400) # Eliminar los valores atípicos de la columna 'LoanAmount'
+  dataframe = drop_outliers(dataframe, 'CoapplicantIncome', max_value = 3000, min_value = 10) # Eliminar los valores atípicos de la columna 'CoapplicantIncome'
   
   X = dataframe.values
   dataframe.to_csv('homeLoanAproval1.csv', index=False)
@@ -146,7 +203,8 @@ def main():
   dataframe = pd.DataFrame(X_hat, columns=dataframe.columns)
   dataframe.to_csv('homeLoanAproval1.csv', index=False)
   naive_bayes(dataframe)
-
+  buildClassificatonTree('homeLoanAproval1.csv', 'LoanStatus_Y')
+  buildKNNeighbors('homeLoanAproval1.csv', 'LoanStatus_Y')
 
 if __name__ == "__main__":
   main()
